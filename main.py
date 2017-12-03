0218f051516a03a6d55453f87bf477418b2f128b
Patricdv <patric.venturini@hotmail.com>
2017-12-03 17:07:29 -0200

    Started the complement of Valid Items.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-class AutomatonLine(object):
-	def __init__(self, composition = []):
-		self.composition = composition
-
 class AutomatonArguments(object):
 	def __init__(self, composition = [], pointPosition = 0, read = 0):
 		self.composition = composition
 		self.pointPosition = pointPosition
 		self.read = read
+
+class ValidItems(object):
+	def __init__(self, automatonProductions = {}, checked = 0, complete = 0):
+		self.automatonProductions = automatonProductions
+		self.checked = checked
+		self.complete = complete
 
 class AutomatonFirstsSet(object):
     def __init__(self, firsts = []):
@@ -25,20 +27,13 @@ class ParsingTable(object):
     def __init__(self, states = {}):
         self.states = states
 
-class ValidItems(object):
-	def __init__(self, automatonProductions = {}, checked = 0, complete = 0):
-		self.automatonProductions = automatonProductions
-		self.checked = checked
-		self.complete = complete
-
 # It's a set of non-Terminal with compositions and productions
-automaton = {}   # Automaton.update({'A': 15})
-noTerminals = [] # noTerminals.append('A')
-validItems = []
-# It's a set of non-Terminal with first set
-automatonFirstsSet = {}
+automaton = {}  	# Automaton.update({'A': 15})
+noTerminals = [] 	# noTerminals.append('A')
+validItems = []  	# validItems.append(ValidItems())
 
-# It's a set of non-Terminal with follow set
+# It's a set of non-Terminal with first set and of non-Terminal with follow set
+automatonFirstsSet = {}
 automatonFollowsSet = {}
 
 # It's a set of valid items with it's compositions
@@ -59,33 +54,55 @@ def makeGrammax(model):
 
 		if (count == 0):
 			noTerminals.append(lineAutomaton+'\'')
-			automaton.update({lineAutomaton+'\'': [AutomatonArguments({1: lineAutomaton + ' $'}, 0, 0)]})
+			automaton.update({lineAutomaton+'\'': [AutomatonArguments([lineAutomaton, '$'], 0, 0)]})
 			count += 1
 
 		for lineComposition in lineCompositions:
-			arguments.append(AutomatonArguments(lineComposition.split(" "), 0, 0))
+			composition = lineComposition.strip().split(" ")
+			arguments.append(AutomatonArguments(composition, 0, 0))
 
 		noTerminals.append(lineAutomaton)
 		automaton.update({lineAutomaton: arguments});
 
-def makeGrammaxFirst():
-    print "making first"
+def iterateInTheDot(validItem):
+	altered = 0
+	for composition in validItem.automatonProductions.keys():
+		print composition
+		for production in validItem.automatonProductions[composition]:
+			if production.read == 1:
+				print "---------------------------------------"
+				print "producao lida"
+				continue
 
-def makeGrammaxFollow():
-    print "making follow"
+			print "Item: ",
+			print production.composition[production.pointPosition]
+			if not production.composition[production.pointPosition]:
+				print "---------------------------------------"
+				print "item completo"
+				production.read = 1
+				validItem.complete = 1
+				continue
+
+			dotItem = production.composition[production.pointPosition]
+			if dotItem not in noTerminals:
+				continue
+
+			if dotItem not in validItem.automatonProductions:
+				validItem.automatonProductions.update({dotItem: automaton[dotItem]})
+				production.read = 1
+				altered = 1
+
+	print altered
+	if altered == 1:
+		iterateInTheDot(validItem)
 
 def makeValidItems():
 	for validItem in validItems:
 		if validItem.checked == 1:
 			continue
 
-		for composition in validItem.automatonProductions:
-			if composition.read == 1:
-				continue
-
-			if composition.arguments[composition.pointPosition] in noTerminals:
-
-			print composition
+		iterateInTheDot(validItem)
+		print validItem.automatonProductions.keys()
 
 	# We'll asume that the first state is the beginning state, so, we have to create the antecessor of the grammax
 	# print "making Valid Items"
@@ -99,6 +116,12 @@ def makeValidItems():
 
 		# print '\n'
 
+
+def makeGrammaxFirst():
+    print "making first"
+
+def makeGrammaxFollow():
+    print "making follow"
 
 def makeGrammaxReductions():
     print "making grammax reductions"
